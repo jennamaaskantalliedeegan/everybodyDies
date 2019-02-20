1010dfb72ea45bda68e374f5666fdcd5c9d7c2c1
Allie <alicemdeegan@gmail.com>
2019-02-20 18:59:12 -0500

    javascript setup

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+//Namespacing our app
+const app = {};
+
+//Variables - do this later
+
+//Function for event listeners (form submit and button to return to form page)
+
+//Assign form values to varibale
+
+//make Ajax request using variables
+
+app.getResult = () => {
+    $.ajax({
+        url: `http://api.population.io:80/1.0/life-expectancy/remaining/female/Canada/2019-02-20/28y7m5d/`,
+        method: "GET",
+        // data: "json",
+        "async": true,
+        "crossDomain": true,
+        "headers": {
+            "cache-control": "no-cache",
+            "Postman-Token": "badd299e-54df-4535-82ce-2da3f2207eb2"
+        }
+    }).then((data) => {
+        console.log(data.remaining_life_expectancy);
+    });
+};
+
+//extract remaining life expectancy from returned data
+
+//convert data into years / months / days / hours / minutes / seconds
+
+//display data on page
+
+// reset form
+
+
+
+
+
+//Create app init!
+app.init = () => {
+    app.getResult();
+};
+
+
+
+//Document Ready
+$(function(){
+    app.init();
+});
