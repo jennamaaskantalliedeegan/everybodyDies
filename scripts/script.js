694c29f8c71ab4b3631620dfcc14a18a1fd658db
Jenna <jenna.maask@gmail.com>
2019-02-20 20:20:20 -0500

    random gender generator

--- scripts/script.js
+++ scripts/script.js
@@ -7,20 +7,32 @@ const app = {};
 app.eventHandler = () => {
     $("form").on("submit", function(e) {
         e.preventDefault();
-        const gender = $("#gender").val();
+        let gender = $("#gender").val();
+        if(gender === "nonBinary" || gender === "unspecified") {
+            gender = app.randomGender();
+        }
         const country = $("#country").val();
         const date = $("#date").val();
         const year = $("#years").val();
         const month = $("#months").val();
         console.log(gender, country, date, year, month);
         app.getResult(gender, country, date, year, month);
-    })
+    });
+}
+
+// function to randomly generate gender
+app.randomGender = () => {
+    number = Math.floor(Math.random()*2 + 1);
+    if( number === 1){
+        return "female";
+    } else {
+        return "male";
+    }
 }
 
 //Assign form values to varibale
 
 //make Ajax request using variables
-
 app.getResult = (gender, country, date, year, month) => {
     $.ajax({
         url: `http://api.population.io:80/1.0/life-expectancy/remaining/${gender}/${country}/${date}/${year}y${month}m/`,
